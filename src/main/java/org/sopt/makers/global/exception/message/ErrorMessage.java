b9ef8243e8baab42c6b4f45621b8f0c2b36ff5e9
DongHoon Lee <dhl1924@naver.com>
2025-04-19 01:35:21 +0900

    feat: Slack 관련 예외 메시지 ErrorMessage enum으로 정의

---
+++ src/main/java/org/sopt/makers/global/exception/message/ErrorMessage.java
@@ -0,0 +1,20 @@
+package org.sopt.makers.global.exception.message;
+
+import org.sopt.makers.global.exception.base.BaseErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorMessage implements BaseErrorCode {
+	SLACK_MESSAGE_BUILD_FAILED(500, "Slack 메시지 생성에 실패했습니다.", "S001"),
+	SLACK_SEND_FAILED(502, "Slack 전송 요청에 실패했습니다.", "S002"),
+	INVALID_SLACK_PAYLOAD(400, "Slack 페이로드 형식이 잘못되었습니다.", "S003"),
+	SLACK_NETWORK_ERROR(503, "Slack 서버 연결에 실패했습니다.", "S004"),
+	SLACK_SEND_INTERRUPTED(500, "Slack 알림 전송이 중단되었습니다.", "S005");
+
+	private final int status;
+	private final String message;
+	private final String code;
+}
