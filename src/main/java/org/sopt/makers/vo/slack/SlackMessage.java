b6b9c007c11c925e350dcdd1817f1360ba70f1f7
DongHoon Lee <dhl1924@naver.com>
2025-04-19 17:44:13 +0900

    fix: Slack 공식 스펙에 맞게 구조화 수정

--- src/main/java/org/sopt/makers/vo/slack/SlackMessage.java
+++ src/main/java/org/sopt/makers/vo/slack/SlackMessage.java
@@ -1,13 +1,20 @@
 package org.sopt.makers.vo.slack;
 
-import org.sopt.makers.vo.slack.block.Block;
 import java.util.List;
 
+import org.sopt.makers.vo.slack.block.Block;
+
 public record SlackMessage(
-	List<Block> blocks,
-	String color
+	List<Attachment> attachments
 ) {
+
 	public static SlackMessage newInstance(List<Block> blocks, String color) {
-		return new SlackMessage(blocks, color);
+		return new SlackMessage(List.of(Attachment.of(color, blocks)));
+	}
+
+	public record Attachment(String color, List<Block> blocks) {
+		public static Attachment of(String color, List<Block> blocks) {
+			return new Attachment(color, blocks);
+		}
 	}
 }
