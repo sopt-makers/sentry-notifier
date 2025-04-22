1e327516a8287ca51978f31caf446116c57b11ff
DongHoon Lee <dhl1924@naver.com>
2025-04-22 16:50:45 +0900

    refactor: 구현체를 이용하도록 리팩터링

--- src/main/java/org/sopt/makers/vo/slack/block/HeaderBlock.java
+++ src/main/java/org/sopt/makers/vo/slack/block/HeaderBlock.java
@@ -1,14 +1,15 @@
 package org.sopt.makers.vo.slack.block;
 
-import org.sopt.makers.vo.slack.text.Text;
-
 import static org.sopt.makers.global.constant.SlackConstant.*;
+
+import org.sopt.makers.vo.slack.text.PlainText;
+import org.sopt.makers.vo.slack.text.Text;
 
 public record HeaderBlock(
 	String type,
 	Text text
 ) implements Block {
 	public static HeaderBlock newInstance(String text) {
-		return new HeaderBlock(BLOCK_TYPE_HEADER, Text.newPlainInstance(text, true));
+		return new HeaderBlock(BLOCK_TYPE_HEADER, PlainText.newInstance("🚨 " + text, true));
 	}
 }
