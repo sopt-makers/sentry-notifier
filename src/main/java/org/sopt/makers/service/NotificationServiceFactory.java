73a62204ff3c4348c7435f19aa95131a5d16d8f6
DongHoon Lee <dhl1924@naver.com>
2025-05-22 00:03:41 +0900

    refactor: ObjectMapper를 인자로 받아 NotificationService를 생성하도록 구조 변경

--- src/main/java/org/sopt/makers/service/NotificationServiceFactory.java
+++ src/main/java/org/sopt/makers/service/NotificationServiceFactory.java
@@ -2,10 +2,12 @@ package org.sopt.makers.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 import org.sopt.makers.global.exception.message.ErrorMessage;
 import org.sopt.makers.global.exception.unchecked.UnsupportedServiceTypeException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -14,22 +16,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationServiceFactory {
-	private static final Map<String, Supplier<NotificationService>> services = new HashMap<>();
+	private static final Map<String, Function<ObjectMapper, NotificationService>> services = new HashMap<>();
 
 	static {
 		registerService("slack", SlackNotificationService::new);
 		registerService("discord", DiscordNotificationService::new);
 	}
 
-	public static NotificationService createService(String serviceType) {
-		Supplier<NotificationService> supplier = services.get(serviceType.toLowerCase());
+	public static NotificationService createService(String serviceType, ObjectMapper objectMapper) {
+		Function<ObjectMapper, NotificationService> supplier = services.get(serviceType.toLowerCase());
 		if (supplier == null) {
 			throw new UnsupportedServiceTypeException(ErrorMessage.UNSUPPORTED_SERVICE_TYPE);
 		}
-		return supplier.get();
+		return supplier.apply(objectMapper);
 	}
 
-	private static void registerService(String type, Supplier<NotificationService> supplier) {
+	private static void registerService(String type, Function<ObjectMapper, NotificationService> supplier) {
 		services.put(type.toLowerCase(), supplier);
 	}
 }
